7a56f8c48a6de3e4488fe4281251c0a1c21de2c3
neilck <neil@chong-kit.com>
2024-09-03 23:10:50 -0700

    updated social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -50,7 +50,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/akaprofiles-social-card.jpg",
     navbar: {
       title: "AKA Profiles",
       logo: {
